26e847456233e7125fe3a1e7b72a7c4310e07690
Somaner <vlokh.t4r4s@gmail.com>
2023-08-02 19:11:48 +0300

    some changes for hosting

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,7 +9,7 @@ const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="https://mongo-posts.onrender.com">
     <Provider store={store}>
       <App />
     </Provider>
